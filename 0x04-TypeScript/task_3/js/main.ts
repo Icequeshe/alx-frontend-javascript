2de206855d0a77cd335fd3f18bfaa9093e883710
Charity Nduta <charitynduta29@gmail.com>
2025-09-11 22:59:55 +0300

    Fix updatedRow in task_3 main.ts by using spread operator

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -10,11 +10,8 @@ const row: RowElement = {
 
 const newRowID: RowID = CRUD.insertRow(row);
 
-const updatedRow: RowElement = {
-  firstName: 'Guillaume',
-  lastName: 'Salva',
-  age: 23,
-};
+// Spread row and add age field
+const updatedRow: RowElement = { ...row, age: 23 };
 
 CRUD.updateRow(newRowID, updatedRow);
 CRUD.deleteRow(newRowID);
