bb5fa9da661cc4fc0cfbc663fff5104b0ba29de1
Charity Nduta <charitynduta29@gmail.com>
2025-09-16 22:02:25 +0300

    Update main.ts

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -35,7 +35,7 @@ export class Teacher implements TeacherInterface {
 }
 
 export function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === "number" && salary < 500) {
+  if (salary < 500) {
     return new Teacher();
   }
   return new Director();
